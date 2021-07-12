50ba9559797464158ba681df027a239f7578b015
Ryan Epp <re17wj@brocku.ca>
2021-07-12 09:13:38 -0400

    Added dynamic updating of old dependency files

--- TSGen.py
+++ TSGen.py
@@ -8,6 +8,7 @@ print("Loading manifest template...\n")
 with open("manifest_template.json", "r+") as f:
     json_data = json.load(f)
 
+## Get general information from user
 json_data["name"] = input("Please input your mod name: ").replace(" ", "_")
 json_data["version_number"] = input("Please input your version number (x.x.x): ")
 json_data["website_url"] = input("Website url? You can leave this blank: ")
@@ -15,30 +16,43 @@ json_data["description"] = input("Write a short description of your mod: ")
 long_desc = input("Input a long description of your mod (this will be displayed on the main page):\n")
 
 print("\nLoading Dependency list...")
-
-if(os.path.exists(os.path.join(os.getcwd(), "Dependencies.json")) == False):
-    try:
-        dep_url = "https://raw.githubusercontent.com/nayr31/TSGen/main/Dependencies.json"
-        dep_file = requests.get(dep_url, stream=True)
-        with open("Dependencies.json", "wb") as f:
-            f.write(dep_file.content)
-    except:
-        print("There was an error downloading/writing the Dependency file.\nPlease run TSGen as admin or download the file manually from the github page.")
-        input()
-        exit
+## Check to see if dependency list is out of date
+try:
+    dep_url = "https://raw.githubusercontent.com/nayr31/TSGen/main/Dependencies.json"
+    dep_file = requests.get(dep_url, stream=True)
+    with open("Dependencies_temp.json", "wb") as f:
+        f.write(dep_file.content)
+except:
+    print("There was an error downloading/writing the Dependency file.\nPlease run TSGen as admin or download the file manually from the github page.")
+    input()
+    exit
 
 with open("Dependencies.json", "r+") as f:
     json_dependencies = json.load(f)
+with open("Dependencies_temp.json", "r+") as f:
+    json_check = json.load(f)
 
+# If its out of date, write the new file
+if(json_dependencies["version"] < json_check["version"]):
+    print("Dependeny list is out of date, updating...")
+    json_dependencies["version"] = json_check["version"]
+    json_dependencies["dependency-name"] = json_check["dependency-name"]
+    json_dependencies["dependency-tag"] = json_check["dependency-tag"]
+    with open("Dependencies.json", "w+") as f:
+        json.dump(json_dependencies, f, indent=2)
+# Always remove old checking file
+os.remove("Dependencies_temp.json")
+
+## Get a list of dependencies from the user
 print("Please list your dependencies by a single number.\nIf a dependency relies on something (otherloader relies on deli), you don't need to put both.")
 
+# Set up dependency array from read file
 dep_pos = 0
-
 for dep_pos in range(0, len(json_dependencies["dependency-name"])):
     print(str(dep_pos) + " - " + json_dependencies["dependency-name"][dep_pos])
-
 print(str(dep_pos + 1) + " - [Stop listing dependencies]")
 
+# Create an empty list and parse the data from the user
 dependency_list = []
 while True:
     dep_input = int(input("\nInput number: "))
@@ -51,13 +65,16 @@ while True:
         break
     else:
         print("That wasn't a valid number from the list, only type the number.")
+# Save that list to the data set
 json_data["dependencies"] = dependency_list
 
+## Create the folder for the files to rest into
 print("Making folder...")
 path = os.path.join(home_dir, json_data["name"])
 os.mkdir(path)
 os.chdir(path)
 
+## Write the data collected into a json, and copy the image file
 print("Attempting to write...")
 with open("manifest.json", "w+") as f:
     json.dump(json_data, f, indent=2)
